2a1e507df4f83153408937ef743cb80981e6fe24
gbsalermo <enzogabrielskull@gmail.com>
2026-02-10 11:01:18 -0300

    Mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,7 +1,9 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 	
@@ -11,7 +13,8 @@ public class ViewController {
 	
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("click"); //Para ele mostrar que cliquei no botao
+		//System.out.println("click"); //Para ele mostrar que cliquei no botao
+		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.INFORMATION);
 	}
 
 }
