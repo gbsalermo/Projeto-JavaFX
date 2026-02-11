067674a90a3438db17abb3ccd9e28861ad24e420
gbsalermo <enzogabrielskull@gmail.com>
2026-02-11 19:59:06 -0300

    ComboBox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,16 +1,23 @@
 package gui;
 
-import java.util.Locale;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-import gui.util.Alerts;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
-public class ViewController {
-	
+public class ViewController  implements Initializable {
+	/* Aula de calculadora com JAVAFX
 	@FXML
 	private TextField txtNumber1;
 	@FXML
@@ -21,7 +28,7 @@ public class ViewController {
 	
 	//Atributo correspondente ao botão
 	/*@FXML ButtonTest
-	private Button btTest;*/
+	private Button btTest;
 	
 	@FXML
 	private Button btSum;
@@ -30,7 +37,7 @@ public class ViewController {
 	public void onBtTestAction() {
 		//System.out.println("click"); //Para ele mostrar que cliquei no botao
 		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.INFORMATION);
-	}*/
+	}
 
 	@FXML
 	public void onBtSumAcion() {
@@ -45,5 +52,52 @@ public class ViewController {
 		Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
 	}
 		
+	}*/
+	
+	@FXML
+	private ComboBox<Person> comboBoxPerson;
+	
+	@FXML
+	private Button btAll;
+	
+	private ObservableList<Person> obsList;
+	
+	@FXML
+	public void onBtAllAction() {
+		for(Person person : comboBoxPerson.getItems()) {
+			System.out.println(person);
+		}
 	}
+	
+	@FXML
+	public void onComboBoxPersonAction() {
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
+		
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		List<Person> list = new ArrayList<>();
+		list.add(new Person(1, "Maria", "maria@gmail.com"));
+		list.add(new Person(2, "Alex", "alex@gmail.com"));
+		list.add(new Person(3, "Bob", "bob@gmail.com"));
+		
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxPerson.setItems(obsList);
+		
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+			super.updateItem(item, empty);
+			setText(empty ? "" : item.getName());
+			}
+			};
+			comboBoxPerson.setCellFactory(factory);
+			comboBoxPerson.setButtonCell(factory.call(null));
+		
+	}
+	
+	
+	
 }
---
+++ src/model/entities/Person.java
@@ -0,0 +1,57 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+	
+	//Atributos
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String email;
+	
+	//Construtores
+	public Person() {
+		
+	}
+
+	public Person(Integer id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	//Getters e setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	//ToString
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+	
+	
+
+}
